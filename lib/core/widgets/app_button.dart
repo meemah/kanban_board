188c8ff3597d5bce85bdb49bf210000c4624677e
meemah <maimunahibro94@gmail.com>
2026-01-06 18:27:59 +0100

    chore: add custom button

---
+++ lib/core/widgets/app_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kanban_board/core/theme/app_colors.dart';
+import 'package:kanban_board/core/theme/app_textstyle.dart';
+
+class AppButton extends StatelessWidget {
+  final String title;
+  final Widget? prefixIcon;
+  final VoidCallback onTap;
+  final double? width;
+  final double? height;
+  final Color? backgroundColor;
+  const AppButton({
+    super.key,
+    required this.title,
+    this.prefixIcon,
+    required this.onTap,
+    this.backgroundColor,
+    this.height,
+    this.width,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: (height ?? 40).h,
+        width: (width ?? double.infinity).w,
+        decoration: BoxDecoration(
+          color: backgroundColor ?? AppColors.primary,
+          borderRadius: BorderRadius.circular(10.r),
+        ),
+        child: Center(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              if (prefixIcon != null) ...[prefixIcon!],
+              Text(
+                title,
+                style: AppTextstyle.bodySemibold(color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
