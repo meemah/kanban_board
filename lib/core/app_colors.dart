5b1e75340bfdf2bbb6a05c91576ced67b323647e
meemah <maimunahibro94@gmail.com>
2026-01-06 17:48:45 +0100

    chore: add default color

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xff137fec);
+  static const Color backgroundLight = Color(0xfff6f7f8);
+  static const Color backgroundDark = Color(0xff101922);
+  static const Color rose = Color(0xffF43F5E);
+
+  static const Color blue500 = Color(0xFF3B82F6);
+
+  static const Color cardDark = Color(0xFF1C252E);
+  static const Color cardLight = Color(0xFFFFFFFF);
+
+  static const Color textDark = Color(0xFF0F172A);
+  static const Color textLight = Color(0xFFFFFFFF);
+
+  static const Color textGray = Color(0xFF64748B);
+
+  static const Color textGrayLight = Color(0xFF9CA3AF);
+  static const Color textGrayDark = Color(0xFF4B5563);
+
+  static const Color borderLight = Color(0xFFE5E7EB);
+  static const Color borderDark = Color(0xFF1F2937);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kanban_board/core/util/font_family.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +11,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Kanban Board', theme: ThemeData());
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: false,
+      builder: (_, child) {
+        return MaterialApp(
+          title: 'Kanban Board',
+          theme: ThemeData(fontFamily: FontFamily.raleway.value),
+          home: SafeArea(child: Scaffold()),
+        );
+      },
+    );
   }
 }
